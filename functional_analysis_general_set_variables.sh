1a03dc1b9e74bf2c75c34f43eeb61e67732d467c
Sarah Aliko <33125090+sarahucl94@users.noreply.github.com>
2020-07-16 21:32:28 +0100

    Update functional_analysis_general_set_variables.sh

--- functional_analysis_general_set_variables.sh
+++ functional_analysis_general_set_variables.sh
@@ -8,47 +8,33 @@
 # Variables currently need to be set up manually
 
 #############################
-# Movie
+# Movie Stimulus
 #############################
 
 # 500 Days of Summer
 movie="500_days"
 movie_length="5470"
 
-# CitizenFour
-# Length = 01:53:24.51 = ((60+53)*60)+2 = 6805
-# I can confirm that the concatenated timeseries = 6805
-#movie="citizenfour"
-#movie_length="6805"
-
-# Split
-# movie="split"
-
 #############################
 # Directories
 #############################
 
 # Base data and data
-# On server:
-base_data_dir="/cluster/project9/brain_graphs"
+base_data_dir="/path/to/dir"
 data_dir="$base_data_dir"/"$movie"
-
-# Stimuli
-# media_dir="$data_dir"/stimulus
 
 #############################
 # Participant(s)
 #############################
 
 # Participant(s) to process
-perps="180303JM"
+participants="XXX"
 
 #############################
 # Degree centrality analysis
 #############################
 
 # NOTE
-# Currently these used for t/sICA also
 # Does dynamic functional connecitvity w/ 3dDegreeCentrality
 
 # File type to use w/ 3dDegreeCentrality
